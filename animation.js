522d7bbfa031467419f746713c20323e07dee801
ITRodis ITRodis <jean-sam222toto.jeansam.tokin@gmail.com>
2024-07-31 00:15:28 +0000

    gsap login interface

---
+++ animation.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.to(".register", { opacity: 1, duration: 1, delay: 0.5 });
+  gsap.to(".logo", { opacity: 1, duration: 1, delay: 1 });
+
+  gsap.from(".logo img", {
+    scale: 0,
+    rotation: 360,
+    duration: 1.5,
+    delay: 1,
+    ease: "elastic.out(1, 0.3)",
+  });
+
+  gsap.from(".sig_log p", {
+    y: -50,
+    opacity: 0,
+    duration: 1,
+    delay: 1.5,
+    stagger: 0.2,
+    ease: "back.out(1.7)",
+  });
+
+  gsap.from("form input", {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    delay: 2,
+    stagger: 0.2,
+    ease: "back.out(1.7)",
+  });
+
+  gsap.from(".agree", {
+    opacity: 0,
+    duration: 1,
+    delay: 2.5,
+    ease: "power2.inOut",
+  });
+
+  gsap.from(".signwithother ul li", {
+    x: -50,
+    opacity: 0,
+    duration: 1,
+    delay: 3,
+    stagger: 0.2,
+    ease: "back.out(1.7)",
+  });
+});
